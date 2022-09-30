3b2c0ee01395ffa43c5d74a5b6c30a8f4bab86a3
Archita <asa410@sfu.ca>
2022-09-30 03:17:22 -0700

    Created an increment button

---
+++ index.js
@@ -0,0 +1,23 @@
+// // document.getElementById("count").innerText=5;
+// let firstBatch =  5
+// let secondBatch = 7
+// let count = firstBatch + secondBatch
+// console.log(count)
+
+// let myAge = 19
+// let humanDogRatio= 7
+// let myDogAge = myAge * humanDogRatio
+// console.log(myDogAge);
+
+let count = 5;
+count = count+2;
+
+console.log(count);
+
+let bonusPoints = 50;
+bonusPoints = bonusPoints+50;
+console.log(bonusPoints);
+bonusPoints = bonusPoints -75;
+console.log(bonusPoints);
+bonusPoints = bonusPoints+45;
+console.log(bonusPoints);
